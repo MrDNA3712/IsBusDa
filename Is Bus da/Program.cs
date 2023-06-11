78ae8d6f70cd8167d3f04ee564d19586826c3986
Jörn Andreesen <jr.andreesen@gmail.com>
2023-06-11 18:03:16 +0200

    neue API

--- Is Bus da/Program.cs
+++ Is Bus da/Program.cs
@@ -15,14 +15,15 @@ namespace Is_Bus_da
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
-            get_departure_info(RWS_stop_id,RWS_dir_id);
+            //get_departure_info(RWS_stop_id,RWS_dir_id);
         }
 
 
 
         // https://v5.bvg.transport.rest/stops/900000037201/departures?direction=900000027305
-        public const string RWS_stop_id = "900000037201";
-        public const string RWS_dir_id = "900000027305";
+        public const string API_URL = "https://v6.bvg.transport.rest";
+        public const string RWS_stop_id = "900037201";
+        public const string RWS_dir_id = "900029302";
 
         static readonly HttpClient httpClient = new HttpClient();
 
@@ -30,14 +31,15 @@ namespace Is_Bus_da
         {
             try
             {
-                string uri = String.Format("https://v5.bvg.transport.rest/stops/{0}/departures?direction={1}&duration=600&results=5", start, direction);
+                string uri = String.Format("{0}/stops/{1}/departures?direction={2}&duration=600&results=5", API_URL, start, direction);
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
                 HttpWebResponse response = (HttpWebResponse)request.GetResponse();
                 Stream stream = response.GetResponseStream();
                 StreamReader reader = new StreamReader(stream);
                 string responseBody = reader.ReadToEnd();
                 Console.WriteLine(responseBody);
-                dynamic liste = JsonConvert.DeserializeObject(responseBody);
+                dynamic json_resp = JsonConvert.DeserializeObject(responseBody);
+                dynamic liste = json_resp["departures"];
                 DepartureInfo[] infos = new DepartureInfo[5];
                 int i = 0;
                 foreach (var item in liste)
